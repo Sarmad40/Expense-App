fdddd81f5baeb940bb370a0fdc59c293d8b315ae
Sarmad40 <sarmadsaeed487@gmail.com>
2026-02-10 23:50:16 +0500

    refactor: Move app to (main) group to fix build error

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+
+import ClientLayout from "@/components/layout/ClientLayout";
+
+export default function MainLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <ClientLayout>
+            {children}
+        </ClientLayout>
+    );
+}
